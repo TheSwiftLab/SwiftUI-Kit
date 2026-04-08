0e4476ef990206917dd132594d5e8479436b71ca
donghyeon kim <69367698+indextrown@users.noreply.github.com>
2026-04-08 13:59:14 +0900

    [#8] 프로젝트 초기 설정을 진행한다 (#9)
    * chore: 초기 폴더 구조 설정
    
    * chore: iOS 최소 버전 설정
    
    * chore: Sample App 생성
    
    * ci: draft pr 이메일 알림 비활성화
    
    * fix: 피드백 반영

---
+++ Sources/SwiftUI-Kit/Native/SKSampleView.swift
@@ -0,0 +1,21 @@
+//
+//  SKSampleView.swift
+//  SwiftUI-Kit
+//
+//  Created by 김동현 on 4/8/26.
+//
+
+import SwiftUI
+
+public struct SKSampleView: View {
+    
+    public init () {}
+    
+    public var body: some View {
+        Text(String(describing: Self.self))
+    }
+}
+
+#Preview {
+    SKSampleView()
+}
