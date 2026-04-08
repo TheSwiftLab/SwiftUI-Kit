262432d0e44d09ed3abbea44ed155bcadad5dafa
indextrown <indextrown@gmail.com>
2026-04-08 13:14:39 +0900

    chore: 초기 폴더 구조 설정

---
+++ Sources/SwiftUI-Kit/Native/SKSampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SwiftUI-Kit
+//
+//  Created by 김동현 on 4/8/26.
+//
+
+import SwiftUI
+
+public struct SKSampleView: View {
+    public var body: some View {
+        Text(String(describing: Self.self))
+    }
+}
+
+#Preview {
+    SKSampleView()
+}
---
+++ Sources/SwiftUI-Kit/Wrapper/SKWebView.swift
@@ -0,0 +1,8 @@
+//
+//  SKWebView.swift
+//  SwiftUI-Kit
+//
+//  Created by 김동현 on 4/8/26.
+//
+
+import Foundation
